c0c6a162a09cae8c41da5792d6fc8bbb42295f36
Russell <Rs0699@cs.ship.edu>
2021-11-24 22:45:12 -0500

    Initial commit

---
+++ Channel11/src/com/shadowlandsmc/channel11/Main.java
@@ -0,0 +1,43 @@
+package com.shadowlandsmc.channel11;
+
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+ public static Main plugin;
+  
+
+
+  // Called when the plugin is enabled. It is used to set up variables and to register things such as commands.
+  @Override
+  public void onEnable() {
+    plugin = getPlugin(Main.class);
+    PluginManager pluginManager = getServer().getPluginManager();
+    
+      getConfig().options().copyDefaults(true); 
+      saveDefaultConfig();
+    
+    /*
+     * Register a command to the list of usable commands. If you don't register the
+     * command, it won't work! Also if you change the command name, make sure to
+     * also change in the plugin.yml file.
+     */
+
+    //pluginManager.registerEvents(new commandProcessor(), this);
+    /*
+     * Make sure you register your listeners if you have any! If you have a class
+     * that implements Listener, you need to make sure to register it. Otherwise it
+     * will DO NOTHING!
+     */
+    pluginManager.registerEvents(new PlayerDeathListener(), this);
+
+    /*
+     * This line lets you send out information to the console. In this case it would
+     * say: Yay, Template-Plugin is now enabled!
+     */
+    this.getLogger().info("Channel 11 news, created by JavaSE");
+  }
+
+}
---
+++ Channel11/src/com/shadowlandsmc/channel11/PlayerDeathListener.java
@@ -0,0 +1,72 @@
+package com.shadowlandsmc.channel11;
+
+import java.util.List;
+import java.util.Random;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.Plugin;
+
+public class PlayerDeathListener implements Listener {
+
+  private static Plugin plugin = Main.plugin;
+  //[Channel 11 news] %announcer% presenting for channel 11 news. %player% was found dead. Police have ruled it a homicide 
+  @SuppressWarnings("unchecked")
+  public List<String> announcer = (List<String>) plugin.getConfig().getList("announcers");
+  
+  
+  @EventHandler
+  public void onPlayerDeathEvent(PlayerDeathEvent event){
+    
+    //remove default death message
+    event.setDeathMessage(null);
+    //killed by custom item
+    if(event.getEntity().getKiller().getInventory().getItemInMainHand() != null) {
+      String msg = plugin.getConfig().getString("msgKilledByCustom");
+      //The way we are replacing color codes is bad practice, and may not work in future updates
+      plugin.getServer().broadcastMessage(msg.replaceAll("&", "§").replaceAll("%announcer%", getAnnoucer()).
+          replaceAll("%player%", event.getEntity().getDisplayName()).replaceAll("%item%", event.getEntity()
+              .getKiller().getInventory().getItemInMainHand().getItemMeta().getDisplayName()).replaceAll("%killer%", 
+                  event.getEntity().getKiller().getName()));
+      
+      //killed by entity, no custom
+    } else {
+      if(event.getEntity().getKiller().getName() != null) {
+        String msg = plugin.getConfig().getString("msg");
+        //The way we are replacing color codes is bad practice, and may not work in future updates
+        plugin.getServer().broadcastMessage(msg.replaceAll("&", "§").replaceAll("%announcer%", getAnnoucer()).
+            replaceAll("%player%", event.getEntity().getDisplayName()).replaceAll("%killer%", event.getEntity().getKiller().getName()));
+        
+        //not killed by entity
+      } else {
+
+        String msg = plugin.getConfig().getString("msgNoKiller");
+        //The way we are replacing color codes is bad practice, and may not work in future updates
+        plugin.getServer().broadcastMessage(msg.replaceAll("&", "§").replaceAll("%announcer%", getAnnoucer()).
+            replaceAll("%player%", event.getEntity().getDisplayName()));
+        
+      }
+
+    }
+  }
+  
+
+  /**
+   * Get the announcer for the news cast
+   * @return String
+   * */
+  public String getAnnoucer() {
+
+    if(announcer.isEmpty() != true) {
+    
+    Random random = new Random();
+    int random_int = random.nextInt(announcer.size());
+    
+    return announcer.get(random_int);
+    }
+    
+    return "generic announcer";
+  }
+  
+}
